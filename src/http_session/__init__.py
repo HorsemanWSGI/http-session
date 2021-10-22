b84cecc5d742430960d0fbcaef0796cb234a70e3
Souheil Chelfouh <trollfot@gmail.com>
2021-10-22 19:25:06 +0200

    Added imports

--- src/http_session/__init__.py
+++ src/http_session/__init__.py
@@ -1,2 +1,11 @@
-from .prototypes import Store, Session
+from .meta import Store, SessionData
+from .session import Session
 from .cookie import SignedCookieManager
+
+
+__all__ = [
+    'Store',
+    'SessionData',
+    'Session',
+    'SignedCookieManager',
+]
